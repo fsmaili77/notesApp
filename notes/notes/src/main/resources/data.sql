a2f6e02701231e09986c8fc2f713af8289cf2a70
fehmir <fehmir.smaili@uha.fr>
2021-10-25 02:01:30 +0200

    Ajout des étudiants, des notes et des matières dans data.sql

--- notes/notes/src/main/resources/data.sql
+++ notes/notes/src/main/resources/data.sql
@@ -1,24 +1,78 @@
-
-
 INSERT INTO matiere(matiere_id, nom_matiere) VALUES (20, "Francais");
 INSERT INTO matiere(matiere_id, nom_matiere) VALUES (21, "Anglais");
 INSERT INTO matiere(matiere_id, nom_matiere) VALUES (22, "Math");
+INSERT INTO matiere(matiere_id, nom_matiere) VALUES (23, "SVT");
+INSERT INTO matiere(matiere_id, nom_matiere) VALUES (24, "Histoire");
+INSERT INTO matiere(matiere_id, nom_matiere) VALUES (25, "Sport");
 
 INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (6, 12, 16, 14, 1, 20);
 INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (7, 14, 11, 12.5, 1, 21);
 INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (8, 18, 15, 16.5, 1, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (9, 8, 12, 10, 1, 23);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (10, 10, 11, 11.5, 1, 24);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (11, 19, 18, 17, 1, 25);
 
-INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (9, 10, 12, 11, 2, 20);
-INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (10, 19, 18, 18.5, 2, 21);
-INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (11, 10, 13, 11.5, 2, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (12, 10, 12, 11, 2, 20);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (13, 19, 18, 18.5, 2, 21);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (14, 10, 13, 11.5, 2, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (15, 10, 11, 11.5, 2, 23);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (16, 16, 18, 17, 2, 24);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (17, 10, 13, 12.5, 2, 25);
 
-INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (12, 17.5, 19, 18, 3, 20);
-INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (13, 15, 12, 13.5, 3, 21);
-INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (14, 20, 19, 19.5, 3, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (18, 11, 11, 11, 3, 20);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (19, 15, 12, 13.5, 3, 21);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (20, 20, 19, 19.5, 3, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (21, 17, 19, 18, 3, 23);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (22, 15, 12, 13.5, 3, 24);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (23, 20, 19, 19.5, 3, 25);
+
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (24, 12, 12, 12, 4, 20);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (25, 14, 12, 13, 4, 21);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (26, 20, 19, 19.5, 4, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (27, 17, 19, 18, 4, 23);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (28, 15, 12, 13.5, 4, 24);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (29, 20, 19, 19.5, 4, 25);
+
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (30, 15, 17, 16, 5, 20);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (31, 12, 14, 13, 5, 21);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (32, 20, 19, 19.5, 5, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (33, 17, 19, 18, 5, 23);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (34, 15, 12, 13.5, 5, 24);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (35, 20, 19, 19.5, 5, 25);
+
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (36, 19, 18, 17, 6, 20);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (37, 10, 12, 11, 6, 21);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (38, 20, 19, 19.5, 6, 22);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (39, 17, 17, 17, 6, 23);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (40, 15, 12, 13.5, 6, 24);
+INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (41, 20, 19, 19.5, 6, 25);
 
 INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (1, "Antoine", "Griezmann", "02-03-1997", "antoine@gmail.fr");
-INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (2, "Kylian", "Mbappé", "15-06-2000", "kylian@gmail.fr");
+INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (2, "Kylian", "Mbappé", "15-06-2000", "kylian@yahoo.fr");
 INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (3, "Karim", "Benzema", "23-05-1995", "karim@gmail.fr");
+INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (4, "Benjamin", "Pavard", "01-08-1999", "benjamin@free.fr");
+INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (5, "Hugo", "Lloris", "12-06-1998", "hugo@hotmail.fr");
+INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (6, "Ngolo", "Kanté", "22-12-1996", "ngolo@sport.fr");
+
+-- INSERT INTO matiere(matiere_id, nom_matiere) VALUES (20, "Francais");
+-- INSERT INTO matiere(matiere_id, nom_matiere) VALUES (21, "Anglais");
+-- INSERT INTO matiere(matiere_id, nom_matiere) VALUES (22, "Math");
+--
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (6, 12, 16, 14, 1, 20);
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (7, 14, 11, 12.5, 1, 21);
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (8, 18, 15, 16.5, 1, 22);
+--
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (9, 10, 12, 11, 2, 20);
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (10, 19, 18, 18.5, 2, 21);
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (11, 10, 13, 11.5, 2, 22);
+--
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (12, 17.5, 19, 18, 3, 20);
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (13, 15, 12, 13.5, 3, 21);
+-- INSERT INTO notes(note_id, note_ecrit, note_oral, note_moyenne, fk_etudiant_id, fk_matiere_id) VALUES (14, 20, 19, 19.5, 3, 22);
+--
+-- INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (1, "Antoine", "Griezmann", "02-03-1997", "antoine@gmail.fr");
+-- INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (2, "Kylian", "Mbappé", "15-06-2000", "kylian@gmail.fr");
+-- INSERT INTO etudiant(etudiant_id, nom, prenom, date_naissance, email) VALUES (3, "Karim", "Benzema", "23-05-1995", "karim@gmail.fr");
 
 
 
